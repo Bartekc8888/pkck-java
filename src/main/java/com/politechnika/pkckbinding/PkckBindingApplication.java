2fc84e4c79a4b84ad88039b0ea5ec3feb3019195
Bartłomiej Ciesielski <bartekc8@op.pl>
2019-06-05 16:26:41 +0200

    Init project

---
+++ src/main/java/com/politechnika/pkckbinding/PkckBindingApplication.java
@@ -0,0 +1,13 @@
+package com.politechnika.pkckbinding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkckBindingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkckBindingApplication.class, args);
+	}
+
+}
